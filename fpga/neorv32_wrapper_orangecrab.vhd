91dc224cf1e02f5196efde6152f53cec0c961d3a
Krystian Hebel <krystian.hebel@3mdeb.com>
2023-10-31 19:43:08 +0100

    neorv32_wrapper_orangecrab.vhd: switch to classic Wishbone
    It is easier to drive ACK signal by AND-ing it with STB, which is not
    possible in pipelined mode.
    
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>

--- fpga/neorv32_wrapper_orangecrab.vhd
+++ fpga/neorv32_wrapper_orangecrab.vhd
@@ -68,10 +68,10 @@ begin
     -- External memory interface (WISHBONE) --
     MEM_EXT_EN                   => true,        -- implement external memory bus interface?
     MEM_EXT_TIMEOUT              => 4096,        -- cycles after a pending bus access auto-terminates (0 = disabled)
-    MEM_EXT_PIPE_MODE            => true,        -- protocol: false=classic/standard wishbone mode, true=pipelined wishbone mode
+    MEM_EXT_PIPE_MODE            => false,       -- protocol: false=classic/standard wishbone mode, true=pipelined wishbone mode
     MEM_EXT_BIG_ENDIAN           => false,       -- byte order: true=big-endian, false=little-endian
-    MEM_EXT_ASYNC_RX             => true,        -- use register buffer for RX data when false
-    MEM_EXT_ASYNC_TX             => true         -- use register buffer for TX data when false
+    MEM_EXT_ASYNC_RX             => false,       -- use register buffer for RX data when false
+    MEM_EXT_ASYNC_TX             => false        -- use register buffer for TX data when false
   )
   port map (
     -- Global control --
