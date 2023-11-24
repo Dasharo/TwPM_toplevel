f216b8b908daa6f38c077bffde6b2c19cf08d942
Artur Kowalski <artur.kowalski@3mdeb.com>
2023-11-24 12:29:20 +0100

    flake.nix: add Zephyr SDK
    Install RISC-V toolchain from Zephyr SDK

--- flake.nix
+++ flake.nix
@@ -42,19 +42,67 @@
           done
         '';
         };
+        zephyrSdk = pkgs.stdenv.mkDerivation rec {
+          pname = "zephyr-sdk";
+          version = "0.16.3";
+          src = builtins.fetchurl {
+            url = "https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v${version}/zephyr-sdk-${version}_linux-x86_64.tar.xz";
+            sha256 = "9eb557d09d0e9d4e0b27f81605250a0618bb929e423987ef40167a3307c82262";
+          };
+          outputs = ["unwrapped" "out"];
+          nativeBuildInputs = with pkgs; [ patchelf makeWrapper ];
+          unpackPhase = ''
+            tar xf $src \
+              zephyr-sdk-${version}/riscv64-zephyr-elf \
+              zephyr-sdk-${version}/cmake \
+              zephyr-sdk-${version}/sdk_version
+          '';
+          installPhase = ''
+            mkdir $out
+            mkdir $unwrapped
+            mv zephyr-sdk-${version}/riscv64-zephyr-elf $unwrapped/
+            mv zephyr-sdk-${version}/cmake $out/
+            mv zephyr-sdk-${version}/sdk_version $out/
+            echo "riscv64-zephyr-elf" > $out/sdk_toolchains
+
+            for file in $(find $unwrapped); do
+              if file $file | grep -qE 'ELF.*executable.*interpreter'; then
+                patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+                  $file
+              fi
+            done
+
+            mkdir -p $out/riscv64-zephyr-elf/bin
+            cd $unwrapped/riscv64-zephyr-elf/bin
+            for file in *; do
+              if [ -x $file ]; then
+                makeWrapper "$unwrapped/riscv64-zephyr-elf/bin/$file" "$out/riscv64-zephyr-elf/bin/$file" \
+                  --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath (with pkgs; [ zlib libmpc gmp mpfr ])}"
+              fi
+            done
+          '';
+        };
+        pythonWithPackages = pkgs.python311.withPackages (ps: with ps; [
+          (west.overridePythonAttrs (super: {
+            propagatedBuildInputs = super.propagatedBuildInputs ++ [ pyelftools ];
+          }))
+        ]);
         packages = with pkgs; [
           yosys
           nextpnr
           trellis ghdl
           dfu-util
           riscv_toolchain_prebuilt
-          gnumake
+          cmake gnumake
+          zephyrSdk pythonWithPackages ninja
+          git
         ];
       in {
         devShells.default = pkgs.mkShellNoCC {
           nativeBuildInputs = packages;
           shellHook = ''
             export PS1="(TwPM) $PS1"
+            export TWPM_ZEPHYR_CMAKE_PATH=${zephyrSdk}/cmake
           '';
         };
         packages.sdk = let
@@ -72,6 +120,7 @@
 
             mkdir /tmp &>/dev/null || true
 
+            export TWPM_ZEPHYR_CMAKE_PATH=${zephyrSdk}/cmake
             exec bash -i
           '';
         in nix2container.buildImage {
