6ef3a44b19db6c3e30694d23576d20f1106eb48c
Krystian Hebel <krystian.hebel@3mdeb.com>
2023-11-27 16:28:46 +0100

    fpga/ram/r512x32_512x32.v: add pin type for consistent symbolator output
    No functional changes.
    
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>

--- fpga/ram/r512x32_512x32.v
+++ fpga/ram/r512x32_512x32.v
@@ -1,13 +1,13 @@
 module r512x32_512x32 (A,RD,WD,Clk,WEN);
 
-input       [8:0] A;
-output reg [31:0] RD;
-input      [31:0] WD;
-input             Clk;
-input       [3:0] WEN;
+input  wire  [8:0] A;
+output reg  [31:0] RD;
+input  wire [31:0] WD;
+input  wire        Clk;
+input  wire  [3:0] WEN;
 
-reg        [31:0] mem [0:511];
-integer           i;
+reg         [31:0] mem [0:511];
+integer            i;
 
 always @(posedge Clk)
   begin
