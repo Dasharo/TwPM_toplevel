155bfc25ab53790ffaa6b68e4bd1faa57c0c9b3c
Artur Kowalski <artur.kowalski@3mdeb.com>
2023-12-08 14:00:00 +0100

    update test code

--- fpga/lpc_test/lpc_test.v
+++ fpga/lpc_test/lpc_test.v
@@ -1,45 +1,62 @@
 module lpc_top (
-    input  wire         clk_48m,
+    input  wire         clk_i,
     input  wire         lclk_i,
-    input  wire         lreset,
     input  wire         lframe_i,
-    inout  wire [  3:0] lad_bus,
-    inout  wire         SERIRQ,
+    inout  wire  [3:0] lad_bus,
     output reg          led_r,
     output reg          led_g,
-    output wire         led_b
+    output wire         led_b,
+    output wire         clk_rising_export
 );
 
 `include "lpc_defines.v"
 
-wire clk_400mhz;
+wire clk_200mhz;
 
-pll_400mhz pll_400mhz_inst (.CLKI(clk_48m), .CLKOP(clk_400mhz));
+pll_main pll_main_inst (.CLKI(clk_i), .CLKOP(clk_200mhz));
 
-/*reg  [ 4:0] fsm_state = `LPC_ST_IDLE;
+reg  [ 4:0] fsm_state = `LPC_ST_IDLE;
 wire [ 4:0] fsm_next_state;
 wire [ 3:0] next_lad_state;
 wire        next_drive_lad;
 reg  [ 3:0] r_lad_state;
 reg         r_drive_lad;
 
+wire        clk_rising;
+wire        clk_falling;
+reg         clk_edge_detect_1;
+reg         clk_edge_detect_2;
 reg         heartbeat = 0;
 reg [ 26:0] heartbeat_div = 0;
 reg [  3:0] tpm_cycle_detected = 0;
 
+assign clk_rising = ~clk_edge_detect_1 & clk_edge_detect_2;
+assign clk_falling = clk_edge_detect_1 & ~clk_edge_detect_2;
+
+// assign clk_rising_export = clk_rising;
+assign clk_rising_export = 0;
+
 // Hearbeat
-always @(posedge clk_i) begin
-    if (heartbeat_div == 0) begin
-        heartbeat_div = 33000000;
-        heartbeat = ~heartbeat;
+always @(posedge clk_200mhz) begin
+    clk_edge_detect_1 <= lclk_i;
+    clk_edge_detect_2 <= clk_edge_detect_1;
 
-        if (tpm_cycle_detected != 0)
-            tpm_cycle_detected = tpm_cycle_detected - 1;
-    end else
-        heartbeat_div = heartbeat_div - 1;
+    if (clk_rising) begin
+        if (heartbeat_div == 0) begin
+            heartbeat_div <= 33000000;
+            heartbeat <= ~heartbeat;
+        end else begin
+            heartbeat_div <= heartbeat_div - 1;
 
-    if (fsm_state == `LPC_ST_ADDR_RD_CLK1) begin
-        tpm_cycle_detected = 3;
+            if (fsm_state == `LPC_ST_ADDR_RD_CLK1)
+                tpm_cycle_detected <= 3;
+        end
+
+
+        r_lad_state <= next_lad_state;
+        r_drive_lad <= next_drive_lad;
+    end else if (clk_falling) begin
+        fsm_state <= fsm_next_state;
     end
 end
 
@@ -68,19 +85,10 @@ assign fsm_next_state = fsm_state == `LPC_ST_IDLE && lframe_i == 0 && lad_bus ==
                       : fsm_state == `LPC_ST_FINAL_TAR_CLK2 ? `LPC_ST_IDLE
                       : `LPC_ST_IDLE;
 
-always @(negedge clk_i) begin
-    fsm_state <= fsm_next_state;
-end
-
-always @(negedge clk_i) begin
-    r_lad_state = next_lad_state;
-    r_drive_lad = next_drive_lad;
-end
-
 assign next_lad_state = fsm_state == `LPC_ST_SYNC_RD ? `LPC_SYNC_READY
-                      : fsm_state == `LPC_ST_DATA_RD_CLK1 ? 4'b0000
+                      : fsm_state == `LPC_ST_DATA_RD_CLK1 ? 4'b1010
                       : fsm_state == `LPC_ST_DATA_RD_CLK2 ? 4'b0000
-                      : fsm_state == `LPC_ST_FINAL_TAR_CLK1 ? 4'b1010
+                      : fsm_state == `LPC_ST_FINAL_TAR_CLK1 ? 4'b1111
                       : 4'b1111;
 
 assign next_drive_lad = fsm_state == `LPC_ST_SYNC_RD ? 1
@@ -90,6 +98,5 @@ assign next_drive_lad = fsm_state == `LPC_ST_SYNC_RD ? 1
                       : 0;
 
 assign lad_bus = r_drive_lad ? r_lad_state : 4'bzzzz;
-//assign lad_bus = r_drive_lad ? 4'b0000 : 4'bzzzz;*/
 
 endmodule
