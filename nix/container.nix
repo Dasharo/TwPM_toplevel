5f5ad29509fb8b0f00279eb8bca61343f9411b3e
Artur Kowalski <artur.kowalski@3mdeb.com>
2024-01-23 17:38:44 +0100

    nix/container.nix: add SDK variants with Litex and Lattice Diamond

--- nix/container.nix
+++ nix/container.nix
@@ -20,11 +20,17 @@
 
       export SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt
       source ${self'.packages.sdk-base}
+
+      if [ -d /etc/twpm ]; then
+        for f in /etc/twpm/*; do
+          source $f
+        done
+      fi
       exec bash -i
     '';
-  in {
-    packages.sdk = nix2container.buildImage {
-      name = "twpm-sdk";
+    buildSdk = name: layers: nix2container.buildImage {
+      inherit name;
+      layers = [ sdkBaseLayer ] ++ layers;
       copyToRoot = [
         (pkgs.buildEnv {
           name = "root";
@@ -51,5 +57,49 @@
         ];
       };
     };
+    sdkBaseLayer = nix2container.buildLayer {
+      deps = containerBasePackages;
+    };
+    mkLayer = { name, deps, addToPath ? true, order ? 99, overlays ? [] }:
+    nix2container.buildLayer {
+      inherit deps;
+      copyToRoot =
+      let
+        overlay = pkgs.writeTextFile {
+          name = "${name}-env-init";
+          text = ''
+            export PATH="${lib.makeBinPath deps}:$PATH"
+          '';
+          destination = "/etc/twpm/${builtins.toString order}-${name}";
+        };
+      in [ overlay ] ++ overlays;
+    };
+    diamondLayer = mkLayer {
+      name = "diamond";
+      deps = [ self'.packages.diamond ];
+      overlays = [
+        (pkgs.runCommand "link-id" {} ''
+          mkdir -p $out/usr/bin
+          cd $out/usr/bin
+          ln -s ${pkgs.coreutils}/bin/id .
+
+          cd ../..
+          mkdir etc
+          cd etc
+          for i in {0..2000}; do
+            echo builder$i:x:$i:$i:Builder $i:/var/empty:/sbin/nologin >> passwd
+          done
+        '')
+      ];
+    };
+    litexLayer = mkLayer {
+      name = "litex";
+      deps = [ self'.packages.litex ];
+    };
+  in {
+    packages.sdk = buildSdk "twpm-sdk" [];
+    packages.sdk-litex = buildSdk "twpm-sdk-litex" [ litexLayer ];
+    packages.sdk-diamond = buildSdk "twpm-sdk-diamond" [ diamondLayer ];
+    packages.sdk-full = buildSdk "twpm-sdk-full" [ litexLayer diamondLayer ];
   };
 }
